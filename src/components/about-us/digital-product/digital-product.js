b1d5dd49d939cede458761efe1fddf8703ff97f6
NazarSemkiv <semkiv.n.i@gmail.com>
2020-03-26 17:52:58 +0200

    rename classes and fix 1 warning

--- src/components/about-us/digital-product/digital-product.js
+++ src/components/about-us/digital-product/digital-product.js
@@ -21,11 +21,11 @@ const DigitalProduct = () => {
   return (
     <div className="digital-product-content">
       <img className="digital-product-content-img" src={data.digitalProductDev.publicURL}/>
-      <div className="digital-product-wrapper container">
-        <div className="digital-product-wrapper-left">
-          <h5 className="digital-product-pre-title">who we are</h5>
-          <h3 className="digital-product-title">Digital product development</h3>
-          <p className="digital-product-text">Golden Ratio Systems is a digital product development and 
+      <div className="digital-product-content-wrapper container">
+        <div className="digital-product-content-wrapper-left">
+          <h5 className="digital-product-content-wrapper-left-pre-title">who we are</h5>
+          <h3 className="digital-product-content-wrapper-left-title">Digital product development</h3>
+          <p className="digital-product-content-wrapper-left-text">Golden Ratio Systems is a digital product development and
             consulting company. Specialize primarily in salesforce.com 
             development. We build scalable complex enterprise grade 
             applications on salesforce that integrate with multiple systems. Golden Ratio Systems is a digital product development and 
@@ -33,10 +33,10 @@ const DigitalProduct = () => {
             development. We build scalable complex enterprise grade 
             applications on salesforce that integrate 
           </p>
-          <p className="digital-product-text">with multiple systems.</p>
+          <p className="digital-product-content-wrapper-left-text">with multiple systems.</p>
         </div>
-        <div className="digital-product-wrapper-right">
-          <img className="digital-product-wrapper-img" src={data.digitalProduct.childImageSharp.fixed.src}/>
+        <div className="digital-product-content-wrapper-right">
+          <img className="digital-product-content-wrapper-right-img" src={data.digitalProduct.childImageSharp.fixed.src}/>
         </div>
       </div>
     </div>
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/shared/layout/layout"
 import SEO from "../components/seo"
