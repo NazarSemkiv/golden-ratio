03d86ece8a6d7a70760b6f9ea3ade1abd1a54baf
andrii_rovenskyi <andrii.rovenskyi@goldenratio.systems>
2020-03-27 18:20:17 +0200

    added new parts for home component

---
+++ src/components/home/salesforce-digital/salesforce-digital.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./salesforce-digital.scss";
+import { graphql, useStaticQuery } from "gatsby";
+
+const SalesforceDigital = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      salesforceDigitalBg: file(relativePath: { eq: "salesforce-digital-bg.svg" }) {
+        publicURL
+      },
+      productDevelopment: file(relativePath: { eq: "product-development.svg" }) {
+        publicURL
+      },
+      salesforceConsultingImg: file(relativePath: { eq: "salesforce-consulting-img.svg" }) {
+        publicURL
+      }
+    }
+  `)
+  return (
+    <div className="salesforce-digital">
+      <img className="salesforce-digital-bg" src={data.salesforceDigitalBg.publicURL}/>
+      <div className="salesforce-digital-container container">
+        <div className="salesforce-digital-left">
+          <img src={data.productDevelopment.publicURL}/>
+          <h3>Salesforce and Digital Product Development</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus aliquam architecto atque beatae ea error est, excepturi fuga harum ipsam mollitia natus provident quibusdam quos ratione, saepe sed ut!</p>
+          <button className="transparent-btn">Learn more</button>
+        </div>
+        <div className="salesforce-digital-right">
+          <img src={data.salesforceConsultingImg.publicURL}/>
+          <h3>Salesforce Consulting</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus aliquam architecto atque beatae ea error est, excepturi fuga harum ipsam mollitia natus provi- dent quibusdam quos ratione, saepe sed ut!</p>
+          <button className="transparent-btn">Learn more</button>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default SalesforceDigital
