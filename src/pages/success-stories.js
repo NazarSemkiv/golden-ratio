991911dc67fb42775c8b344ad3f60d4bc94d2ab4
NazarSemkiv <semkiv.n.i@gmail.com>
2020-03-30 17:53:26 +0300

    success stories - comment section (3/4)

--- src/pages/success-stories.js
+++ src/pages/success-stories.js
@@ -4,11 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/shared/layout/layout"
 import SEO from "../components/seo"
 import Intro from "../components/success-stories/intro/intro"
+import Comments from '../components/success-stories/comments/comments';
 
 const SuccessStoriesPage = () => (
   <Layout>
     <SEO title="Success Stories" />
     <Intro/>
+    <Comments/>
   </Layout>
 )
 
