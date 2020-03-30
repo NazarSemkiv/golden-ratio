1e028b548e0d74662d0a12e5d3074f6e59f8a7e5
NazarSemkiv <semkiv.n.i@gmail.com>
2020-03-30 20:23:01 +0300

    success stories - our family section (4/4)

--- src/pages/success-stories.js
+++ src/pages/success-stories.js
@@ -1,16 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/shared/layout/layout"
 import SEO from "../components/seo"
 import Intro from "../components/success-stories/intro/intro"
 import Comments from '../components/success-stories/comments/comments';
+import OurFamily from '../components/success-stories/our-family/our-family';
 
 const SuccessStoriesPage = () => (
-  <Layout>
+  <Layout page={"stories"}>
     <SEO title="Success Stories" />
     <Intro/>
     <Comments/>
+    <OurFamily/>
   </Layout>
 )
 
