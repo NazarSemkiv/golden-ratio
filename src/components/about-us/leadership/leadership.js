a4e7d945248498eae59659200699aa6096edc5ff
andrii_rovenskyi <andrii.rovenskyi@goldenratio.systems>
2020-04-08 11:10:04 +0300

    added responsive for home and about-us

--- src/components/about-us/leadership/leadership.js
+++ src/components/about-us/leadership/leadership.js
@@ -49,36 +49,36 @@ const Leadership = () => {
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 1,
-    autoplay: true,
+    //autoplay: true,
     speed: 1000,
     pauseOnHover: true,
     autoplaySpeed: 2000,
     arrows: true,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: true,
-    //       dots: true,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 600,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    // ],
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
     return (
--- src/components/shared/header/header.js
+++ src/components/shared/header/header.js
@@ -1,51 +1,62 @@
-import React from "react";
+//import React from "react";
+import React, { Component } from 'react';
 import {Link} from "gatsby";
 import "./header.scss"
-import { graphql, useStaticQuery } from "gatsby";
+//import { graphql, useStaticQuery } from "gatsby";
 
-const Header = () => {
-
-  const data = useStaticQuery(graphql`
-  query {
-    logoGoldenRatio: file(relativePath: { eq: "logo-golden-ratio.svg" }) {
-      publicURL
-    },
-    goldenRationSystemBackground: file(relativePath: { eq: "golden-ration-system-background.svg" }) {
-      publicURL
-    }
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.openHamburger = this.openHamburger.bind(this);
+    this.state = {
+      condition: false,
+    };
   }
-  `)
 
-  return (
-    <header className="header">
-      <div className="inner-header-content">
-        <div className="logo-img">
-          <img src={data.logoGoldenRatio.publicURL}/>
+  openHamburger() {
+    this.setState({
+      condition: !this.state.condition
+    });
+  }
+
+  render() {
+  
+    return (
+      <header className="header">
+        <div className="inner-header-content">
+          <div className="logo-img">
+            {/* <img src={data.logoGoldenRatio.publicURL}/> */}
+          </div>
+          <div className="header-links">
+            <button className= { this.state.condition ? "hamburger hamburger--collapse is-active" : "hamburger hamburger--collapse" } type="button" onClick={this.openHamburger}>
+              <span className="hamburger-box">
+                <span className="hamburger-inner"></span>
+              </span>
+            </button>
+            <ul className="links-list" ref="myDiv">
+              <li className="links-list-li">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="links-list-li">
+                <Link to="/about-us/">About</Link>
+              </li>
+              <li className="links-list-li">
+                <Link to="/services/">Services</Link>
+              </li>
+              <li className="links-list-li">
+                <Link to="/product/">Products</Link>
+              </li>
+              <li className="links-list-li">
+                <Link to="/success-stories/">Success Stories</Link>
+              </li>
+              <li className="links-list-li">Blog</li>
+              <li className="links-list-li">Contact</li>
+            </ul>
+          </div>
         </div>
-        <div className="header-links">
-          <ul className="links-list">
-            <li className="links-list-li">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="links-list-li">
-              <Link to="/about-us/">About</Link>
-            </li>
-            <li className="links-list-li">
-              <Link to="/services/">Services</Link>
-            </li>
-            <li className="links-list-li">
-              <Link to="/product/">Products</Link>
-            </li>
-            <li className="links-list-li">
-              <Link to="/success-stories/">Success Stories</Link>
-            </li>
-            <li className="links-list-li">Blog</li>
-            <li className="links-list-li">Contact</li>
-          </ul>
-        </div>
-      </div>
-    </header>
-  )
+      </header>
+    )
+  }
 }
 
 export default Header
