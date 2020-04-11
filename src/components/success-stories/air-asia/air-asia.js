76bb188058a2f7c95b711a336e8476bdb11d7717
andrii_rovenskyi <andrii.rovenskyi@goldenratio.systems>
2020-04-11 22:38:14 +0300

    added success stories responsive

--- src/components/success-stories/air-asia/air-asia.js
+++ src/components/success-stories/air-asia/air-asia.js
@@ -27,38 +27,62 @@ const AirAsia= () => {
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
+    //autoplay: true,
     speed: 1000,
     pauseOnHover: true,
     autoplaySpeed: 2000,
     arrows: true,
     variableWidth: true,
     centerPadding: '-367px',
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: true,
-    //       dots: true,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 600,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //     },
-    //   },
-    // ],
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1430,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+          centerPadding: '-430px',
+          //centerMode: true
+        },
+      },
+      {
+        breakpoint: 1346,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false,
+          centerPadding: '-10px',
+          //centerMode: true
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '150px'
+        },
+      },
+      {
+        breakpoint: 894,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '95px'
+        },
+      },
+      {
+        breakpoint: 723,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '5px'
+        },
+      },
+    ],
   };
 
   return (
